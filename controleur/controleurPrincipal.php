b473f11f99d0e9f135e73acfb00b646706284948
Chazam <beuquila.jeremy@gmail.com>
2017-10-06 17:00:43 +0200

    conflit encore ptn

--- controleur/controleurPrincipal.php
+++ controleur/controleurPrincipal.php
@@ -5,7 +5,6 @@ require_once 'library/menu.php';
 require_once 'library/dispatcher.php';
 require_once 'library/formulaire.php';
 require_once 'modele/dao.php';
-//require_once 'modele/dto.php';
 require_once 'modele/DAO/select.php';
 require_once 'modele/DTO/resto.php';
 
