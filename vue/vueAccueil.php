05a376672bf60466d5f76c346f356dde691335fd
Deadpoulpe <32497179+Deadpoulpe@users.noreply.github.com>
2017-10-06 16:38:17 +0200

    accueil

--- vue/vueAccueil.php
+++ vue/vueAccueil.php
@@ -3,13 +3,9 @@
       <?php include 'haut.php' ;?>
   </header>
 
-  <main>
+
       <div id="accueil">
 
       </div>
-  </main>
 
-  <footer>
-      <?php include 'bas.php' ;?>
-  </footer>
 </div>
