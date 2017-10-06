8176890d9343a07e2a28d9c6ee99c533d9ca97a1
SBelondrade <31722469+SBelondrade@users.noreply.github.com>
2017-10-06 14:45:14 +0200

    modif select

--- modele/DAO/select.php
+++ modele/DAO/select.php
@@ -1,44 +1,55 @@
 <?php
-
-public function selectUser($table)
+class UserDAO
 {
-  $sql = "SELECT NOMU, PRENOMU FROM " . $table . ";";
-}
-
-public function selectPlatResto($idResto)
-{
-  $sql = "SELECT NOMP, PRIXFOURNISSEURP, PRIXCLIENTP, PLATVISIBLE, PHOTOP, DESCRIPTIONP FROM PLAT WHERE IDR = '" . $idResto . "';";
-  foreach ($cnx->query($sql) as $tablo)
+  public function selectUser($table)
   {
-    printf(['NOMP']);
-    printf(['PRIXFOURNISSEURP']);
-    printf(['PRIXCLIENTP']);
-    printf(['PLATVISIBLE']);
-    printf(['PHOTOP']);
-    printf(['DESCRIPTIONP']);
+    $sql = "SELECT NOMU, PRENOMU FROM " . $table . ";";
   }
 
 }
 
-public function selectFindIDResto($resto)
+
+class RestoDAO
 {
-  $sql = "SELECT IDR FROM resto WHERE NOMR = ''" . $resto . "'';";
+  public function selectPlatResto($idResto)
+  {
+    $sql = "SELECT NOMP, PRIXFOURNISSEURP, PRIXCLIENTP, PLATVISIBLE, PHOTOP, DESCRIPTIONP FROM PLAT WHERE IDR = '" . $idResto . "';";
+    foreach ($cnx->query($sql) as $tablo)
+    {
+      printf(['NOMP']);
+      printf(['PRIXFOURNISSEURP']);
+      printf(['PRIXCLIENTP']);
+      printf(['PLATVISIBLE']);
+      printf(['PHOTOP']);
+      printf(['DESCRIPTIONP']);
+    }
+
+  }
+
+  public function selectFindIDResto($resto)
+  {
+    $sql = "SELECT IDR FROM resto WHERE NOMR = ''" . $resto . "'';";
+  }
+
+  public function selectListeResto()
+  {
+    $sql = "SELECT * FROM resto;";
+  }
+
+  public function selectEvaluationResto($idR)
+  {
+    $sql = "SELECT IDU, NOTERAPIDITE, NOTEQUALITE, NOTETEMP, NOTECOUT, COMMENTAIRE, COMVISIBLE FROM evaluer WHERE IDR ='" . $idR . "';";
+
+  }
 }
 
-public function selectCommande($idUser)
+class CommandeDAO
 {
-  $sql = "SELECT IDR, DATEC, COMMENTAIRECLIENTC, DATELIVRC, MODEREGLEMENTC, AVISCLIENT, NOTECLIENT, COMVISIBLE FROM commande WHERE IDC = $idUser";
+  public function selectCommande($idUser)
+  {
+    $sql = "SELECT IDR, DATEC, COMMENTAIRECLIENTC, DATELIVRC, MODEREGLEMENTC, AVISCLIENT, NOTECLIENT, COMVISIBLE FROM commande WHERE IDC = $idUser";
 
+  }
 }
 
-public function selectListeResto()
-{
-  $sql = "SELECT * FROM resto;";
-}
-
-public function selectEvaluationResto($idR)
-{
-  $sql = "SELECT IDU, NOTERAPIDITE, NOTEQUALITE, NOTETEMP, NOTECOUT, COMMENTAIRE, COMVISIBLE FROM evaluer WHERE IDR ='" . $idR . "';";
-
-}
  ?>
