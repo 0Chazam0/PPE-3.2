3a2e032935d6c4a520649de0b8bb8a297145fb18
Chazam <beuquila.jeremy@gmail.com>
2017-10-06 14:28:53 +0200

    DAO connex et query

---
+++ controleur/controleurMdpOublie.php
@@ -0,0 +1,1 @@
+<?php include "vue\vueMdpOublie.php.php"; ?>
--- vue/vueConnexion.php
+++ vue/vueConnexion.php
@@ -4,7 +4,34 @@
   </header>
 
   <main>
-      Connexion
+    <?php if (isset($_SESSION["Identifiant"])){
+      $txt ="	<div class='contentConnexion'>
+          <div class='btn'>";
+      $txt .= "<form method='post' action='index.php'>
+        <div class='ligne'>
+        <input type = 'submit' value = 'Deconnecter'/>
+        <input id ='deco' type = 'hidden' name='deco' value=''/>
+        </div>";
+      $txt .= "</form>
+      </div></div>";
+      echo $txt;
+    }
+    else {
+        echo "<form method='post' action='index.php'>
+          <div class='contentConnexion'>
+            <div class='btnIdent'>
+                  <input id ='id' type = 'text' placeholder='Saisir votre identifiant' name='id' value=''/>
+                  <br/>
+                  <br/>
+                  <input id='mdp' type = 'password' placeholder='Saisir votre code' name='mdp' value=''/><br/><br/>
+                  <input id = 'validCo' type = 'submit' value = 'Valider'/>
+                  <input type = 'reset' value ='Annuler'/>
+              </div>
+          </div>
+          </form>
+          <a class ='mdpOublie' href='controleur/controleurMdpOublie.php'>Mot depasse oublié ? </a>";
+    }
+    ?>
   </main>
 
   <footer>
