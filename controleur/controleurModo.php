63d7ea7fa124ee78546e84a5d785fbab8514fa1b
SBelondrade <31722469+SBelondrade@users.noreply.github.com>
2017-10-12 11:43:35 +0200

    evaluer

---
+++ controleur/controleurModo.php
@@ -0,0 +1,8 @@
+<?php
+require_once 'modele/DAO/select.php';
+require_once 'modele/DTO/evaluer.php';
+
+print_r(CommentaireDAO::selectCommentaire());
+
+
+ ?>
---
+++ vue/vueModo.php
@@ -0,0 +1,21 @@
+<div class="conteneur">
+<header>
+      <?php include 'haut.php' ;?>
+  </header>
+  <img src="image\background.jpg" width="100%" height="100%" style="position:absolute;">-->
+  <div id="txtModo">
+    <p></p>
+
+  </div>
+  <div class="gestModo">'
+  <?php
+
+      // $formRecherche = new Formulaire("POST","#","formModo","searchthis");
+      //
+      // $formRecherche->creerFormulaire();
+      // echo $formRecherche->afficherFormulaire();
+    ?>
+    <!-- <script type="text/javascript" src="fonction\autocomplete.js"></script> -->
+  </div>
+
+</div>
